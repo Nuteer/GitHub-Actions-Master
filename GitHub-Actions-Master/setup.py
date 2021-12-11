a51646d8651bcd53b782fda5c19ff15abb371a62
Bruno Nuteer Cupido <nuteer@hotmail.com>
2021-12-11 16:20:01 -0300

    git hubactions

---
+++ GitHub-Actions-Master/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sample_project',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hands-on session on Github Actions',
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pre-commit==2.7.1"
+        ]
+    }
+)
